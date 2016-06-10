9da1a313c8613c3d0d557b389db5b9e5f02685a0
jmueloschat <jmueloschat@gmail.com>
2016-06-10 19:27:17 -0300

    update

--- voncell/src/view/Voncell.java
+++ voncell/src/view/Voncell.java
@@ -3,9 +3,10 @@ package view;
 public class Voncell {
 
 	public static void main(String[] args) {
-		System.out.println("Vison on Cell");
+		System.out.println("Vison on Cell        ");
 		System.out.println("by juliano mueloschat");
-		System.out.println("- - - - - - - - - - - -");
+		System.out.println("- - - - - - - - - - -");
+		System.out.println("- - - - - - - - - - -");
 	}
 
 }
