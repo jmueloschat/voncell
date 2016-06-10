cf76f40e0ef64e63dd6435882d7c92ffcbbc60f3
jmueloschat <jmueloschat@gmail.com>
2016-06-10 19:12:17 -0300

    Change-Id: I1c5d71c75e6fd58ec18242ee26293a86a7107adc

---
+++ voncell/src/view/Voncell.java
@@ -0,0 +1,10 @@
+package view;
+
+public class Voncell {
+
+	public static void main(String[] args) {
+		System.out.println("Vison on Cell");
+		System.out.println("by juliano mueloschat");
+	}
+
+}
